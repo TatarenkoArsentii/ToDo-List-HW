607b948f37c570985696634f0df2443521d225b4
Arsentii Tatarenko <tatarenkoarsen@gmail.com>
2021-09-27 23:31:37 +0300

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,43 @@
 "use strict";
-const body = document.body;
-const todoListInput = document.querySelector('input[name="toDo_Descr"]');
-const todoListSubmitBtn = document.querySelector("#todo_list_submit");
-const ul = document.querySelector(".toDo_list");
+const todoListFormInput = document.querySelector(
+  'input[name="toDo_list_input"]'
+);
+const todoListFormSubmitBtn = document.querySelector("#todo_list_form_submit");
+const toDoListUl = document.querySelector(".toDo_list");
 const TODO_LIST = "TODO_LIST";
-// ul.addEventListener("click", deleteTask);
-todoListSubmitBtn.addEventListener("click", createList);
+const saveBtn = document.querySelector("#save");
+const clearBtn = document.querySelector("#clear");
+const deleteChckBtn = document.querySelector("#delete_checked");
 
-function createList() {
+todoListFormSubmitBtn.addEventListener("click", createToDoList);
+saveBtn.addEventListener("click", saveToDoList);
+clearBtn.addEventListener("click", clearToDoList);
+deleteChckBtn.addEventListener("click", deleteChecked);
+
+function createToDoList() {
   event.preventDefault();
-  const liItem = document.createElement("li");
+  const listItems = document.createElement("li");
   const timeDiv = document.createElement("div");
   const descrDiv = document.createElement("div");
   const deleteBtn = document.createElement("button");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.setAttribute("value", "done");
+  checkbox.setAttribute("name", "isDone");
   timeDiv.classList.add("time");
   descrDiv.classList.add("todo_list_item_descr");
   deleteBtn.id = "todo_list_item_delete";
   timeDiv.innerText = getTime();
-  descrDiv.innerText = todoListInput.value;
-  liItem.append(timeDiv, descrDiv, deleteBtn);
-  liItem.classList.add("toDo_list_items");
+  descrDiv.innerText = todoListFormInput.value;
+  listItems.append(timeDiv, descrDiv, checkbox, deleteBtn);
+  listItems.classList.add("toDo_list_items");
 
-  if (todoListInput.value == "") {
+  if (todoListFormInput.value == "") {
     return;
   }
-  
-  ul.append(liItem);
-  todoListInput.value = "";
-  const elem = getDeleteElements();
-  elem.forEach((el) => {
-    el.addEventListener("click", deleteTask);
-  });
+  toDoListUl.append(listItems);
+  todoListFormInput.value = "";
+  deleteToDoElements();
 }
 
 function getTime() {
@@ -38,11 +45,63 @@ function getTime() {
   return time;
 }
 
-function getDeleteElements() {
-  const deleteBtnElems = document.querySelectorAll("#todo_list_item_delete");
-  return deleteBtnElems;
+function doneListItem() {
+  const listItems = document.querySelectorAll(".toDo_list_items");
+  listItems.forEach((el) =>
+    el.addEventListener("click", function () {
+      el.classList.add("done");
+    })
+  );
 }
 
-function deleteTask(elem) {
-  elem.target.parentElement.remove();
+function getCheckBoxes() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  return checkboxes;
 }
+
+function deleteChecked() {
+  const checkboxes = getCheckBoxes();
+  checkboxes.forEach((el) => {
+    if (el.checked) {
+      el.parentElement.remove();
+    }
+  });
+}
+
+function deleteToDoElements() {
+  const deleteBtnElems = document.querySelectorAll("#todo_list_item_delete");
+
+  deleteBtnElems.forEach((elem) =>
+    elem.addEventListener("click", function () {
+      elem.parentElement.remove();
+    })
+  );
+}
+
+function saveToDoList() {
+  if (toDoListUl.innerHTML == "") {
+    alert("Список пуст");
+  } else {
+    localStorage.setItem(TODO_LIST, toDoListUl.innerHTML);
+    alert("Список успешно сохранен");
+  }
+  doneListItem();
+}
+
+function clearToDoList() {
+  if (toDoListUl.innerHTML != "") {
+    const confirmMsg = confirm("Вы уверены что хотите очистить список?");
+    if (confirmMsg == true) {
+      localStorage.removeItem(TODO_LIST);
+      toDoListUl.innerHTML = "";
+    }
+  }
+}
+
+function getToDoList() {
+  toDoListUl.innerHTML = localStorage.getItem(TODO_LIST);
+}
+
+getToDoList();
+deleteToDoElements();
+doneListItem();
